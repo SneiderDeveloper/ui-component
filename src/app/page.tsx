cd0512427baf9039677b591386643ce435da15b5
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2023-12-22 00:26:46 -0500

    Starting ligth table component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,52 @@
 import { Search } from '@/components/Search/Search'
+import { TableSimple } from '@/components/Table/Table'
 
 export default function Home() {
+  const rows = [
+    {
+      key: "1",
+      name: "Tony Reichert",
+      role: "CEO",
+      status: "Active",
+    },
+    {
+      key: "2",
+      name: "Zoey Lang",
+      role: "Technical Lead",
+      status: "Paused",
+    },
+    {
+      key: "3",
+      name: "Jane Fisher",
+      role: "Senior Developer",
+      status: "Active",
+    },
+    {
+      key: "4",
+      name: "William Howard",
+      role: "Community Manager",
+      status: "Vacation",
+    },
+  ];
+  
+  const columns = [
+    {
+      key: "name",
+      label: "NAME",
+    },
+    {
+      key: "role",
+      label: "ROLE",
+    },
+    {
+      key: "status",
+      label: "STATUS",
+    },
+  ]; 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Search placeholder='Types of potatoes' label='Search'/>
+      <TableSimple rows={ rows } columns={ columns }/>
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/components/input.js',
+    './node_modules/@nextui-org/theme/dist/components/(input|table).js',
   ],
   theme: {
     extend: {
