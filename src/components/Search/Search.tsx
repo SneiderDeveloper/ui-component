9c68fa3d809667aa845601fc47d805f428eda7a2
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2023-12-21 19:58:00 -0500

    Start of search component

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,90 @@
+'use client'
+
+import { Input } from '@nextui-org/input';
+import { SearchIcon } from './SearchIcon'
+import { useSearchParams, useRouter, usePathname } from 'next/navigation'
+import { useState } from 'react'
+
+export const Search = ({
+    label,
+    placeholder,
+}: {
+    label?: string
+    placeholder?: string;
+}) => {
+    const searchParams = useSearchParams()
+    const pathName = usePathname()
+    const { replace } = useRouter()
+    const [searchValue, setSearchValue] = useState(searchParams.get('search') || '');
+    
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const params = new URLSearchParams(searchParams)
+        const value = e.target.value
+        setSearchValue(value)
+
+        if (value) {
+            params.set('search', value)
+        } else {
+            params.delete('search')
+        }
+
+        replace(`${pathName}?${params.toString()}`)
+    }
+
+    const handleClear = () => {
+        const params = new URLSearchParams(searchParams)
+        params.delete('search')
+        replace(`${pathName}?${params.toString()}`)
+        setSearchValue('')
+    }
+
+    const styles = {
+        label: 'text-black/50 dark:text-white/90',
+        input: [
+        'bg-transparent',
+        'text-black/90 dark:text-white/90',
+        'placeholder:text-default-700/50 dark:placeholder:text-white/60',
+        ],
+        innerWrapper: 'bg-transparent',
+        inputWrapper: [
+        'shadow-xl',
+        'bg-default-200/50',
+        'dark:bg-default/60',
+        'backdrop-blur-xl',
+        'backdrop-saturate-200',
+        'hover:bg-default-200/70',
+        'dark:hover:bg-default/70',
+        'group-data-[focused=true]:bg-default-200/50',
+        'dark:group-data-[focused=true]:bg-default/60',
+        '!cursor-text',
+        ],
+    }
+
+    return (
+        <search className='w-[340px]'>
+            <Input
+                label={ label }
+                isClearable
+                value={ searchValue }
+                onClear={ handleClear }
+                radius='lg'
+                onChange={ handleChange }
+                defaultValue={ searchParams.get('search') || '' }
+                classNames={ styles }
+                placeholder={ placeholder }
+                startContent={
+                    <SearchIcon 
+                        className='
+                            text-black/50 
+                            mb-0.5 
+                            dark:text-white/90 
+                            text-slate-400 
+                            pointer-events-none 
+                            flex-shrink-0
+                        ' 
+                    />
+                }
+            />
+        </search>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from 'tailwindcss'
+import { nextui } from '@nextui-org/theme'
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/components/input.js',
   ],
   theme: {
     extend: {
@@ -15,6 +17,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: 'class',
+  plugins: [nextui()],
 }
 export default config
