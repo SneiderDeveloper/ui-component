857e1501b7f914f3bc6b83c2b7f3ebfd25f099c6
josuebedoya <josuebedoya348@gmail.com>
2024-04-29 10:53:50 -0500

    add: fornt-end | delete tipos from  Interface: AvataProps/Avatar.tsx

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -2,23 +2,9 @@ import { Avatar as AvatarContainer, AvatarGroup } from "@nextui-org/avatar";
 import { ReactElement } from "react";
 
 interface AvatarProps {
-  src?: string;
-  alt?: string;
-  name?: string;
-  size?: number;
-  className?: string;
-  isDisabled?: boolean;
-  isBordered?: boolean;
-  showFallback?: boolean;
-  fallback?: {};
-  radius?: string;
-  color?: string;
   isAvatarGroup?: boolean;
   avatarGroup?: {};
-children?: ReactElement;
-  max?: number;
-  total?: number;
-  isGrid?: boolean;
+  children?: ReactElement;
 }
 
 const Avatar: React.FC<AvatarProps> = ({
